2dc372cb6f137ceb19db415da368deb29d0a6623
Kei Takashima <kei.takashima@crssoftware.co.nz>
2023-05-19 12:14:53 +1200

    trying goterm

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+
+	tm "github.com/buger/goterm"
+)
+
+func main() {
+	tm.Clear() // Clear current screen
+
+	for {
+		// By moving cursor to top-left position we ensure that console output
+		// will be overwritten each time, instead of adding new.
+		tm.MoveCursor(1, 1)
+
+		tm.Println("Current Time:", time.Now().Format(time.RFC1123))
+
+		tm.Flush() // Call it every time at the end of rendering
+
+		time.Sleep(time.Second)
+	}
+}
