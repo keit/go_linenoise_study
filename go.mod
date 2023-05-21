1c4c8bac01bfe5a05fa3048d4089af19e1183187
Kei Takashima <kei.takashima@crssoftware.co.nz>
2023-05-21 16:59:02 +1200

    import psql

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/keit/goterm_study
 
 go 1.19
 
-require github.com/GeertJohan/go.linenoise v0.0.0-20141120151038-1918ff89d613
+require (
+	github.com/GeertJohan/go.linenoise v0.0.0-20141120151038-1918ff89d613
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 
 	linenoise "github.com/GeertJohan/go.linenoise"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "keitakashima"
+	password = "password" // even you don't need it, you must pass.
+	dbname   = "cmr_core_development"
 )
 
 func main() {
-	fmt.Println("Welcome to go.linenoise example.")
-	writeHelp()
+	db := initDB()
+	// close database
+	defer db.Close()
+
+	linenoise.SetMultiline(true)
+
 	for {
-		str, err := linenoise.Line("prompt> ")
+		str, err := linenoise.Line("gsql> ")
 		if err != nil {
 			if err == linenoise.KillSignalError {
 				quit()
@@ -20,94 +35,78 @@ func main() {
 			fmt.Printf("Unexpected error: %s\n", err)
 			quit()
 		}
-		fields := strings.Fields(str)
 
-		// check if there is any valid input at all
-		if len(fields) == 0 {
-			writeUnrecognized()
-			continue
+		err = linenoise.AddHistory(str)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
 		}
-
-		// switch on the command
-		switch fields[0] {
-		case "help":
-			writeHelp()
-		case "echo":
-			fmt.Printf("echo: %s\n\n", str[5:])
-		case "clear":
-			linenoise.Clear()
-		case "multiline":
-			fmt.Println("Setting linenoise to multiline")
-			linenoise.SetMultiline(true)
-		case "singleline":
-			fmt.Println("Setting linenoise to singleline")
-			linenoise.SetMultiline(false)
-		case "complete":
-			fmt.Println("Setting arguments as completion values for linenoise.")
-			fmt.Printf("%d arguments: %s\n", len(fields)-1, fields[1:])
-			completionHandler := func(in string) []string {
-				return fields[1:]
-			}
-			linenoise.SetCompletionHandler(completionHandler)
-		case "printKeyCodes":
-			linenoise.PrintKeyCodes()
-		case "addHistory":
-			if len(str) < 12 {
-				fmt.Println("No argument given.")
-			}
-			err := linenoise.AddHistory(str[11:])
-			if err != nil {
-				fmt.Printf("Error: %s\n", err)
-			}
-		case "save":
-			if len(fields) != 2 {
-				fmt.Println("Error. Expecting 'save <filename>'.")
-				continue
-			}
-			err := linenoise.SaveHistory(fields[1])
-			if err != nil {
-				fmt.Printf("Error on save: %s\n", err)
-			}
-		case "load":
-			if len(fields) != 2 {
-				fmt.Println("Error. Expecting 'load <filename>'.")
-				continue
-			}
-			err := linenoise.LoadHistory(fields[1])
-			if err != nil {
-				fmt.Printf("Error on load: %s\n", err)
-			}
-		case "quit":
+		if str == "quit" {
 			quit()
-		default:
-			writeUnrecognized()
+		}
+		doQuery(db, str)
+
+	}
+
+}
+
+func doQuery(db *sql.DB, query string) {
+	fmt.Printf("Query: %s\n", query)
+	rows, err := db.Query(query)
+	if err != nil {
+		fmt.Printf("Error Query: %s\n", err)
+	} else {
+		defer rows.Close()
+		columnTypes, err := rows.ColumnTypes()
+		CheckError(err)
+		fmt.Println("Column Name, Type, Length, Nullable")
+		for _, v := range columnTypes {
+			// do something
+			fmt.Printf("%s %s %s %s\n", v.Name(), v.DatabaseTypeName(), fmtColLength(v), fmtColNullable(v))
 		}
 	}
 }
 
+func fmtColLength(col *sql.ColumnType) string {
+	length, ok := col.Length()
+	if ok {
+		return strconv.FormatInt(length, 10)
+	} else {
+		return "-"
+	}
+}
+
+func fmtColNullable(col *sql.ColumnType) string {
+	nullable, ok := col.Nullable()
+	if ok {
+		return strconv.FormatBool(nullable)
+	} else {
+		return "-"
+	}
+}
+
+func initDB() *sql.DB {
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+
+	return db
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 func quit() {
-	fmt.Println("Thanks for running the go.linenoise example.")
 	fmt.Println("")
 	os.Exit(0)
-}
-
-func writeHelp() {
-	fmt.Println("help              write this message")
-	fmt.Println("echo ...          echo the arguments")
-	fmt.Println("clear             clear the screen")
-	fmt.Println("multiline         set linenoise to multiline")
-	fmt.Println("singleline        set linenoise to singleline")
-	fmt.Println("complete ...      set arguments as completion values")
-	fmt.Println("addHistory ...    add arguments to linenoise history")
-	fmt.Println("save <filename>   save the history to file")
-	fmt.Println("load <filename>   load the history from file")
-	fmt.Println("quit              stop the program")
-	fmt.Println("")
-	fmt.Println("Use the arrow up and down keys to walk through history.")
-	fmt.Println("Note that you have to use addHistory to create history entries. Commands are not added to history in this example.")
-	fmt.Println("")
-}
-
-func writeUnrecognized() {
-	fmt.Println("Unrecognized command. Use 'help'.")
 }
